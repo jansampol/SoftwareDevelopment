452ddcd0425f1d9b8f51c648c3310ec4bb7189d3
jansampol <jansampolramirez00@gmail.com>
2025-02-17 13:28:05 +0100

    Creation of assignment 2, first subassignemnt done

---
+++ 07-SDfR-assignment2/assignment2/src/print_maze.cpp
@@ -0,0 +1,24 @@
+//==============================================================
+// Filename : print_maze.cpp
+// Authors : Jan Sampol and Teresa Edo
+// Group : 7
+// License : N.A. or open source license like LGPL
+// Description : Definition of the print_maze function, which takes as input the defined maze and prints it.
+//==============================================================
+
+#include <iostream>
+#include <array>
+#include <string>
+#include "../include/maze.h"  // Include the header file
+
+// Function definition (no template)
+void print_maze(const std::array<std::array<std::string, COLS>, ROWS>& maze) {
+    std::cout << '\n';
+    for (const auto& row : maze) {
+        for (const auto& item : row) {
+            std::cout << item << ' ';
+        }
+        std::cout << '\n';
+    }
+    std::cout << '\n';
+}
